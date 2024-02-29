c5fcf8f44d2b732328dfad3212b7a49ad517a80f
PC <PC@INFO5-2561>
2024-02-29 08:46:56 +0100

    feat: funzione StampaVotiPesi

--- Program.cs
+++ Program.cs
@@ -5,6 +5,10 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Riccardo Guardabascio 3G PSArray");
+            Console.WriteLine("Inserire il numero di voti");
+            int nVoti=Convert.ToInt32(Console.ReadLine());
+            double[] voti=new double[nVoti];
+            int[] pesi=new int[nVoti];
             Console.WriteLine("I voti con in relativi pesi:");
             StampaVotiPesi(voti, pesi, nVoti);
 
@@ -12,10 +16,12 @@
         static void StampaVotiPesi(double[] voti, int[] pesi, int nVoti)
         {
             Console.WriteLine("Voti     Pesi");
-            for(int i=0; i<voti.Length; i++)
+            for(int i=0; i<nVoti; i++)
             {
-                Console.WriteLine(voti[i]+       pesi[i]);
+                Console.WriteLine($"{voti[i]}      {pesi[i]}");
             }
         }
+
+        
     }
 }
